df5ba30037c54150eb40fca5877499ac3c0d7087
James He <he.is.james7@gmail.com>
2024-04-20 23:23:30 -0700

    updated user model

--- repeat/db_model.py
+++ repeat/db_model.py
@@ -1,15 +1,44 @@
 import reflex as rx
 from typing import List
 
-import sqlmodel
-import sqlalchemy
+from sqlmodel import Field 
+
+class Health_Record(rx.Model):
+    """Model of Health Records"""
+    HDL_Cholesterol: float
+    LDL_Cholesterol: float
+    Triglycerides: float
+    Glucose: float
+    Sodium: float
+    Potassium: float
+    Chloride: float
+    Carbon_dioxide: float
+    Calcium: float
+    Protein: float
+    Lipoprotein: float
+
+class Ingredients(rx.Model):
+    """Model of Ingredients"""
+    ingredient: str
+    quantity: float
+    unit: str
+
+class Recipe(rx.Model):
+    """Model of Recipes"""
+    name: str
+    time: str
+    ingredients: List[Ingredients]
+    instructions: List[str]
+
 
 class User(rx.Model, table=True):
     """A table of Users."""
 
-    username: str
+    username: str = Field(primary_key=True)
     password: str
-    # ingredients: List[str] = sqlmodel.Field(
-    #     default=None,
-    #     sa_column=sqlalchemy.Column(sqlalchemy.ARRAY(sqlalchemy.String)),
-    # )
+    inventory_ingredients: List[str]
+    age: int
+    gender: str
+    weight: str
+    health_record: Health_Record
+    recipes: List[Recipe]
